8660eb123b6b517e34e971aa52dddcb19dbbb21e
kiana-nb <kiana.nb34@gmail.com>
2025-02-25 09:44:39 +0330

    fix: ui enhancements

--- src/app/days/[day]/tasks/_components/day-banner.tsx
+++ src/app/days/[day]/tasks/_components/day-banner.tsx
@@ -1,10 +1,14 @@
+import VectorFish1 from "@/vectors/fish1";
 import React from "react";
 
 export default function DayBanner() {
   return (
     <div className="flex flex-col justify-center items-center gap-4">
-      <h1 className="text-[#2763B0] font-[700] text-[18px]">روز دوم</h1>
-      <h1 className="text-[#2763B0] font-[700] text-[18px]">روز سبزه‌های جادویی</h1>
+      <div className="flex flex-col items-center justify-center">
+        <VectorFish1 className="z-10"/>
+        <h1 className="text-[#2763B0] bg-white rounded-full text-center py-1 px-2.5 -mt-1 mr-6 border-r-2 border-b-2 border-[#2763B0] font-[600] text-[14px]">روز دوم</h1>
+      </div>
+      <h2 className="text-[#2763B0] font-[700] text-[18px]">روز سبزه‌های جادویی</h2>
     </div>
   );
 }
