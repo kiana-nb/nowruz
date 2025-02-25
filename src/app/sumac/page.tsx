832f47a8428dc225b95c00d65de0cbc352df30d1
Par431160 <parastoorahmani1380@gmail.com>
2025-02-25 15:45:40 +0330

    feat: add sumac and grass page

---
+++ src/app/sumac/page.tsx
@@ -0,0 +1,29 @@
+import VectorBranchOfFlower from "@/vectors/branch-of-flower";
+import Image from "next/image";
+
+export default function page() {
+  return (
+    <div className="bg-[url('/watercolor-bg1.svg')] w-screen h-dvh bg-no-repeat bg-cover relative overflow-hidden">
+      <div className="overflow-y-scroll h-full w-full px-5 py-10 gap-20 flex flex-col items-center">
+        <div className="absolute min-w-[300px] min-h-[200px] top-10 translate-y-1/4 text-sm font-[600] left-1/2 -translate-x-1/2 text-[#744C3D] bg-[#D9D9D9] p-3 rounded-[20px] flex text-center justify-center"></div>
+        <div className="absolute top-1/2 grid grid-cols-1 gap-4 my-1">
+          <div className="bg-white border rounded-[20px] border-[#6F1717] min-h-12 min-w-72"></div>
+        </div>
+      </div>
+      <div className="absolute bottom-20 rotate-180 left-0">
+        <VectorBranchOfFlower />
+      </div>
+      <div className="absolute bottom-0 z-10 right-0">
+        <VectorBranchOfFlower />
+      </div>
+
+      <Image
+        src="/sumac.svg"
+        alt=""
+        width={170}
+        height={100}
+        className="absolute bottom-6 right-0"
+      />
+    </div>
+  );
+}
