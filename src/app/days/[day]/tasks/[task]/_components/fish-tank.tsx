e12497ace808841a4ab6912bcd57339d6eae5abc
kiana-nb <kiana.nb34@gmail.com>
2025-02-26 09:27:04 +0330

    fix: style issues

--- src/app/days/[day]/tasks/[task]/_components/fish-tank.tsx
+++ src/app/days/[day]/tasks/[task]/_components/fish-tank.tsx
@@ -8,18 +8,22 @@ export default function FishTank() {
   return (
     <div className="bg-[url('/watercolor-bg1.svg')] w-screen h-dvh bg-no-repeat bg-cover relative overflow-hidden">
       <div className="absolute right-4 top-4 flex gap-4 items-center">
-                    <BackButton />
-                    <ProgressBar className="bg-[#FAC821] border-[#F19C25]" icon={<TinyFish/>} progress={50}/>
-                  </div>
-      <div className="overflow-y-scroll h-full w-full px-5 py-10 gap-20 flex flex-col items-center">
-        <div className="absolute min-w-[300px] min-h-[50px] top-20 translate-y-1/4 text-sm font-[600] left-1/2 -translate-x-1/2 text-[#DC8D1D] bg-white p-3 rounded-[20px] flex  justify-center">علی یه جعبه مدادرنگی داره، ۳ تاشو داده به دوستش، چندتا براش مونده؟</div>
-        <div className="absolute top-48 grid grid-cols-2 gap-4 my-1">
+        <BackButton />
+        <ProgressBar className="bg-[#FAC821] border-[#F19C25]" icon={<TinyFish />} progress={50} />
+      </div>
+      <section className="flex flex-col gap-5  items-center overflow-y-scroll h-full w-full px-5 py-10 pt-20">
+        <div className="p-4 text-sm font-[600] text-[#DC8D1D] bg-white rounded-[20px] flex  justify-center">
+          علی یه جعبه مدادرنگی داره، ۳ تاشو داده به دوستش، چندتا براش مونده؟
+        </div>
+
+        <div className="grid grid-cols-2 gap-4">
           <div className="bg-white border rounded-[20px] border-[#C1C1C1] min-h-20 min-w-32 shadow-[inset_4px_4px_10px_0px_#0000000A]"></div>
           <div className="bg-white border rounded-[20px] border-[#C1C1C1] min-h-20 min-w-32 shadow-[inset_4px_4px_10px_0px_#0000000A]"></div>
           <div className="bg-white border rounded-[20px] border-[#C1C1C1] min-h-20 min-w-32 shadow-[inset_4px_4px_10px_0px_#0000000A]"></div>
           <div className="bg-white border rounded-[20px] border-[#C1C1C1] min-h-20 min-w-32 shadow-[inset_4px_4px_10px_0px_#0000000A]"></div>
         </div>
-      </div>
+      </section>
+
       <div className="absolute bottom-0 rotate-180 left-0">
         <VectorBranchOfFlower />
       </div>
@@ -27,13 +31,7 @@ export default function FishTank() {
         <VectorBranchOfFlower />
       </div>
 
-      <Image
-        src="/fish-tank.svg"
-        alt=""
-        width={170}
-        height={100}
-        className="absolute bottom-4 left-1/2 -translate-x-1/2"
-      />
+      <Image src="/fish-tank.svg" alt="" width={170} height={100} className="absolute bottom-4 left-1/2 -translate-x-1/2" />
     </div>
   );
 }
