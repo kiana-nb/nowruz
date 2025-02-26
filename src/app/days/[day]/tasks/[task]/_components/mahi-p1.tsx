c33f1dc5ed62ddf3b10f32c194ed2f615c074731
Amirreza <amirrezakryml@gmail.com>
2025-02-26 10:51:50 +0330

    feat: add new component

--- src/app/days/[day]/tasks/[task]/_components/mahi-p1.tsx
+++ src/app/days/[day]/tasks/[task]/_components/mahi-p1.tsx
@@ -3,11 +3,12 @@ import VectorBranchOfFlower from "@/vectors/branch-of-flower";
 import Image from "next/image";
 import ProgressBar from "../../_components/progress-bar";
 import TinyFish from "@/vectors/tiny-fish";
+import IconSeda from "@/icons/icon-seda";
 
-export default function Sumac() {
+export default function MahiP1() {
   return (
     <div className="bg-[url('/watercolor-bg1.svg')] w-screen h-dvh bg-no-repeat bg-cover relative overflow-hidden">
-      <div className=" flex w-full p-4 gap-x-4 items-center">
+      <div className="flex w-full p-4 gap-x-4 items-center">
         <BackButton />
         <ProgressBar
           className="bg-[#FAC821] border-[#F19C25]"
@@ -15,25 +16,29 @@ export default function Sumac() {
           progress={50}
         />
       </div>
+
       <div className="px-5 gap-10 flex flex-col items-center">
-        <div className=" min-w-[300px] min-h-[200px] text-sm font-[600] text-[#744C3D] bg-[#D9D9D9] p-3 rounded-[20px] flex text-center justify-center"></div>
-        <div className="grid grid-cols-1 gap-4 my-1">
-          <div className="bg-white border rounded-[20px] border-[#6F1717] min-h-12 min-w-72"></div>
+        <div className="bg-white flex items-center rounded-[20px] w-full shadow p-4"></div>
+        <IconSeda />
+        <p className="text-sm font-medium">
+          میتونی برامون یه شعر از بهار بخونی؟
+        </p>
+        <div className="min-w-[300px] min-h-[180px] text-sm font-[600] text-[#744C3D] bg-[#D9D9D9] p-3 rounded-[20px] flex text-center justify-center">
+          54
         </div>
       </div>
-      <div className="absolute bottom-20 rotate-180 left-0">
+      <div className="absolute bottom-0 rotate-180 left-0">
         <VectorBranchOfFlower />
       </div>
-      <div className="absolute bottom-0 z-10 right-0">
+      <div className="absolute bottom-8 z-10 right-0">
         <VectorBranchOfFlower />
       </div>
-
       <Image
-        src="/sumac.svg"
+        src="/fish-tank.svg"
         alt=""
         width={170}
         height={100}
-        className="absolute bottom-6 right-0"
+        className="absolute bottom-2 left-1/2 -translate-x-1/2"
       />
     </div>
   );
--- src/app/days/[day]/tasks/[task]/_components/mirror.tsx
+++ src/app/days/[day]/tasks/[task]/_components/mirror.tsx
@@ -6,18 +6,13 @@ import TinyFish from "@/vectors/tiny-fish";
 export default function Mirror() {
   return (
     <div className="bg-[url('/watercolor-bg1.svg')] w-screen h-dvh bg-no-repeat bg-cover relative overflow-hidden">
-      <div className="absolute right-4 top-4 flex gap-4 items-center">
-                    <BackButton />
-                    <ProgressBar className="bg-[#FAC821] border-[#F19C25]" icon={<TinyFish/>} progress={50}/>
-                  </div>
-      <div className="overflow-y-scroll h-full w-full px-5 py-10 gap-20 flex flex-col items-center">
-        <div className="flex flex-col justify-center items-center absolute top-1/2 gap-6 my-5">
-          <div className="bg-[#FAFFB2] h-32 w-72  rounded-[21px]"></div>
-          <div className="flex flex-col gap-3">
-            <div className="bg-[#FAFFB2] h-10 w-72 p-4 rounded-[21px]"></div>
-            <div className="bg-[#FAFFB2] h-10 w-72 p-4 rounded-[21px]"></div>
-          </div>
-        </div>
+      <div className=" flex w-full p-4 gap-x-4 items-center">
+        <BackButton />
+        <ProgressBar
+          className="bg-[#FAC821] border-[#F19C25]"
+          icon={<TinyFish />}
+          progress={50}
+        />
       </div>
 
       <Image
@@ -25,8 +20,17 @@ export default function Mirror() {
         alt=""
         width={300}
         height={200}
-        className="absolute top-16 left-1/2 -translate-x-1/2 "
+        className="mx-auto mb-3"
       />
+      <div className=" px-5 gap-10 flex flex-col items-center">
+        <div className="flex flex-col justify-center items-center gap-6 ">
+          <div className="bg-[#FAFFB2] h-32 min-w-72  rounded-[21px]"></div>
+          <div className="flex flex-col gap-3">
+            <div className="bg-[#FAFFB2] h-10 w-72 p-4 rounded-[21px]"></div>
+            <div className="bg-[#FAFFB2] h-10 w-72 p-4 rounded-[21px]"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
