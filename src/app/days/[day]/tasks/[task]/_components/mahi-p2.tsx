f50b7e0164999955169a865d0065afb2d806fd64
Amirreza <amirrezakryml@gmail.com>
2025-02-26 11:28:40 +0330

    feat: add new component

--- src/app/days/[day]/tasks/[task]/_components/mahi-p2.tsx
+++ src/app/days/[day]/tasks/[task]/_components/mahi-p2.tsx
@@ -5,7 +5,7 @@ import ProgressBar from "../../_components/progress-bar";
 import TinyFish from "@/vectors/tiny-fish";
 import IconSeda from "@/icons/icon-seda";
 
-export default function MahiP1() {
+export default function MahiP2() {
   return (
     <div className="bg-[url('/watercolor-bg1.svg')] w-full min-h-dvh bg-no-repeat bg-cover relative overflow-hidden">
       <div className="absolute right-4 top-4 flex gap-4 items-center">
@@ -23,15 +23,11 @@ export default function MahiP1() {
           </div>
           <p>میتونی برامون یه شعر از بهار بخونی؟</p>
         </div>
-        <div className="p-4 w-full  gap-2 items-center text-sm font-[600] shadow bg-white rounded-[20px] flex flex-col ">
-          <div className="bg-[#45B4DB4D] w-full min-h-28 rounded-[28px]"></div>
-
-          <div className="w-full flex items-center justify-end">
-            <button className="flex items-center bg-[#FAC821] rounded-[19px] text-white p-2">
-              <p>ارسال فایل</p>
-              <IconSeda />
-            </button>
+        <div className="p-4 w-full items-center text-sm font-[600] text-[#fff] bg-[#FAC821] rounded-[20px] flex gap-x-2">
+          <div className="bg-[#fff] rounded-full p-2">
+            <IconSeda />
           </div>
+          <p>ضبط صدا</p>
         </div>
       </section>
 
